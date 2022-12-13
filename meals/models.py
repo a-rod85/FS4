2b61f5e1c4d4e8c9b5c5564d411e5dc02e7fe08d
a-rod85 <31aaronanderson@gmail.com>
2022-12-13 17:00:14 +0000

    add pillow add to meals app

--- meals/models.py
+++ meals/models.py
@@ -3,9 +3,14 @@ from django.db import models
 # Create your models here.
 
 
-class Meals(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField(max_length=500)
-    people = models.IntegerField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    image = models.ImageField(upload_to='meals/')
+def jls_extract_def(max_length, max_digits, decimal_places, upload_to):
+    class Meals(models.Model):
+        name = models.CharField(max_length=50)
+        description = models.TextField(max_length=500)
+        people = models.IntegerField()
+        price = models.DecimalField(max_digits=5, decimal_places=2)
+        image = models.ImageField(upload_to='meals/')    return image
+
+
+image = jls_extract_def(max_length, max_digits, decimal_places, upload_to)
+
