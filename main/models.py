4b0f9b9fc80c94cd9d6329594cadf891ad227277
a-rod85 <31aaronanderson@gmail.com>
2022-12-20 14:45:09 +0000

    add def to models.py save, add slug, add slugify

--- main/models.py
+++ main/models.py
@@ -1,15 +1,22 @@
 from django.db import models
-
+from django.utils.text import slugify
 # Create your models here.
 
 
 class Meals(models.Model):
-        name = models.CharField(max_length=50)
-        description = models.TextField(max_length=500)
-        people = models.IntegerField()
-        price = models.DecimalField(max_digits=5, decimal_places=2)
-        image = models.ImageField(upload_to='meals/')
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    people = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(upload_to='meals/')
+    slug = models.SlugField(blan=True, null=True)
 
 
-        def __str__(self):
-            return self.name
+def save(self, *args, **kwargs):
+    if not self.slug and self.name:
+        self.slug = slugify(self.name)
+    super(Meals, self).save(*args, **kwargs)
+
+
+def __str__(self):
+    return self.name
