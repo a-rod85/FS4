2acf2aaadc66a1126cfbd2fc1f33b2d61f1dd06b
a-rod85 <31aaronanderson@gmail.com>
2022-12-21 14:07:45 +0000

    corrected deletion on urls.py add book_your_table.html add to admin

--- main/models.py
+++ main/models.py
@@ -3,7 +3,25 @@ from django.utils.text import slugify
 # Create your models here.
 
 
-class Meals(models.Model):
+class Reservations(models.Model):
+    name = models.CharField(default= 'None', max_length=30)
+    Restaurant = models.ForeignKey("Restaurant", on_delete=models.PROTECT)
+    Booking_time = models.DateTimeField(null=False, blank=False, unique=True)
+    def __str__(self):
+        return self.name
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+
+class Table(models.Model):
+    restaurant = models.ForeignKey("Restaurant", on_delete = models.PROTECT) 
+    table_number = models.CharField(max_length= 2, default= "0")
+    def __str__(self):
+        return self.table_number      
+
+class Menu(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=500)
     people = models.IntegerField()
---
+++ menu/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def meal_list(request):
+    pass
+
+
+def meal_detail(request):
+    pass
